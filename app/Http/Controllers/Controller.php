b7c4e7da797a8771d3552620cc5892db93012d91
satheesh kumar <4798470+dreamstudio-satheesh@users.noreply.github.com>
2023-10-16 19:43:53 +0530

    add laravel ui

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -9,4 +9,53 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    public $successStatus = 200;
+
+    public function sendResponse($result, $message)
+    {
+        $response = [
+            'success' => true,
+            'data'    => $result,
+            'message' => $message,
+        ];
+
+        return response()->json($response, $this->successStatus);
+    }
+
+    public function sendError($error, $errorMessages = [], $code = 404)
+    {
+        $response = [
+            'success' => false,
+            'message' => $error,
+        ];
+
+        if (!empty($errorMessages)) {
+            $response['data'] = $errorMessages;
+        }
+
+        return response()->json($response, $code);
+    }
+    
+    public function sendSuccess($message)
+    {
+        $response = [
+            'success' => true,
+            'message' => $message,
+        ];
+
+        return response()->json($response, $this->successStatus);
+    }
+
+    public function sendSuccessWithData($message, $data)
+    {
+        $response = [
+            'success' => true,
+            'message' => $message,
+            'data'    => $data,
+        ];
+
+        return response()->json($response, $this->successStatus);
+    }
+
 }
