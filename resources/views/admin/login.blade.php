7629059ed382daa83525e9d2e35d3b3bf2aa9b3f
satheesh kumar <4798470+dreamstudio-satheesh@users.noreply.github.com>
2023-10-19 03:18:11 +0530

    update login  theme

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -1,7 +1,39 @@
-@extends('layouts.app')
+@extends('admin.layouts.sign')
 
 @section('content')
-<div class="container">
+
+    <h4 class="text-dark mb-5">Sign In</h4>
+    
+    <form action="/index.html">
+      <div class="row">
+        <div class="form-group col-md-12 mb-4">
+          <input type="email" class="form-control input-lg" id="email" aria-describedby="emailHelp" placeholder="Username">
+        </div>
+
+        <div class="form-group col-md-12 ">
+          <input type="password" class="form-control input-lg" id="password" placeholder="Password">
+        </div>
+
+        <div class="col-md-12">
+          <div class="d-flex my-2 justify-content-between">
+            <div class="d-inline-block mr-3">
+              <label class="control control-checkbox">Remember me
+                <input type="checkbox" />
+                <div class="control-indicator"></div>
+              </label>
+            </div>
+
+            <p><a class="text-blue" href="#">Forgot Your Password?</a></p>
+          </div>
+
+          <button type="submit" class="btn btn-lg btn-primary btn-block mb-4">Sign In</button>
+
+          
+        </div>
+      </div>
+    </form>
+  
+{{-- <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -69,5 +101,5 @@
             </div>
         </div>
     </div>
-</div>
+</div> --}}
 @endsection
