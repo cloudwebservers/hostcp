322b88a77034e4ce3e7c30498ddc81a44a43a763
beatjansath <janusath210@gmail.com>
2023-10-20 21:40:57 +0530

    client create

--- resources/views/admin/clients/create.blade.php
+++ resources/views/admin/clients/create.blade.php
@@ -5,7 +5,35 @@
 
 @section('content')
 
+<div class="content">
+    <div class="row">
+        <div class="col-6">
+            <div class="card card-default">
+
+                <div class="card-header card-header-border-bottom">
+                    <h2>Masked Input</h2>
+                </div>
+
+                <div class="card-body">
+
+                </div>
+            </div>
+        </div>
+        <div class="col-6">
+            <div class="card card-default">
+
+                <div class="card-header card-header-border-bottom">
+                    <h2>Masked Input</h2>
+                </div>
+
+                <div class="card-body">
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
+
+
 @endsection
-
-
-
