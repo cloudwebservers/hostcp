7b068de275caf987537f68e2d275b8412c5869ce
beatjansath <janusath210@gmail.com>
2023-10-20 20:52:34 +0530

    remove customer seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call(CustomersTableSeeder::class);
+       
         $this->call(AdminsTableSeeder::class);
     }
 }
